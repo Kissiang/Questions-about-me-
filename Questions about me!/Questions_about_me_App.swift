b9c23359a0a9a5e838631b0e80d262d40a038e67
Kissian Griffin <griffink9451@gmail.com>
2023-06-09 10:29:57 -0500

    Initial Commit

---
+++ Questions about me!/Questions_about_me_App.swift
@@ -0,0 +1,17 @@
+//
+//  Questions_about_me_App.swift
+//  Questions about me!
+//
+//  Created by Scholar on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Questions_about_me_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
